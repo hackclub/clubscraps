eeac3be7d07e0c91ee37dfabda71b66b4f7c574a
Cedric Hutchings <ced@misguided.enterprises>
2022-02-28 16:09:47 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { App } = require('@slack/bolt');
+const fs = require("fs");
+const { botToken, signingSecret } = JSON.parse(fs.readFileSync("auth.json"));
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
